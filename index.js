056f2ccdd4ba99f3bfa10907b229825e380ee0a0
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2023-11-04 20:02:59 +0600

    mongodb connection added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 // middleware
@@ -15,6 +16,27 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+
+//connect mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@job-house.p1cwnsp.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const dbConnect = async () => {
+  try {
+    client.connect();
+    console.log("DB Connected Successfully✅");
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+};
+dbConnect();
 
 // home apis route
 app.get("/", (req, res) => {
