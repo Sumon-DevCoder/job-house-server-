a4d4e286f2df05ab4353bc38b27ef02f1d4e4e97
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2023-11-04 19:54:10 +0600

    express server setup completed

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// home apis route
+app.get("/", (req, res) => {
+  res.send("server is running...");
+});
+
+app.listen(port, () => {
+  console.log(`server is running successfully at http://${port}`);
+});
